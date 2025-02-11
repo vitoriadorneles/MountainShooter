4f4e930a041e3f14fcedf2dfad38c94537d7210e
Vitoria Dorneles <vitorianrdorneles@outlook.com>
2025-02-11 19:04:55 -0300

    adding classes generated from StarUML

---
+++ MountainShooter/code/class1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Player import Player
+
+
+class Class1(Player):
+    pass
---
+++ MountainShooter/code/player.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Player:
+    def __init__(self):
+        pass
+
+    def mone(self, ):
+        pass
