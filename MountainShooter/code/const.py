0620bc203111562c7262bb7e0cd76abc2362f845
Vitoria Dorneles <vitorianrdorneles@outlook.com>
2025-02-14 19:30:25 -0300

    Changing menu

---
+++ MountainShooter/code/const.py
@@ -0,0 +1,15 @@
+#C
+C_ORANGE = (255, 128, 0)
+C_YELLOW = (235, 255, 159)
+C_WHITE = (255, 255, 255)
+
+#M
+MENU_OPTION = ('MENU GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- MountainShooter/code/game.py
+++ MountainShooter/code/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
@@ -9,17 +10,13 @@ from code.menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))  # making a window (x,y)
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))  # making a window (x,y)
 
-    def run(self, ):
+    def run(self):
         while True:  # keeping the window open
             menu = Menu(self.window)
             menu.run()
             pass
 
 
-            # check for all events
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit()  # close window
-            #        quit()  # end pygame
+
