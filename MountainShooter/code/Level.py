6b0a6c1cbc2f1fbd3d77cd19cad0449437f0ef98
Vitoria Dorneles <vitorianrdorneles@outlook.com>
2025-03-08 15:58:52 -0300

    Creating Mediator Pattern. Enemies Destruction.

--- MountainShooter/code/Level.py
+++ MountainShooter/code/Level.py
@@ -11,10 +11,7 @@ from pygame.locals import Rect
 from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
-
-
-class EntityMediator:
-    pass
+from code.EntityMediator import EntityMediator
 
 
 class Level:
@@ -52,6 +49,10 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            # collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
+        pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
